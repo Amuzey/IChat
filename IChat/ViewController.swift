590934fb2895060bec2d374ace100f656e456086
Amuzey <ivanovad97@gmail.com>
2022-09-06 14:53:31 +0500

    adding canvas

--- IChat/ViewController.swift
+++ IChat/ViewController.swift
@@ -14,7 +14,25 @@ class ViewController: UIViewController {
         
         view.backgroundColor = .red
     }
-
-
 }
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            viewController
+        }
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+    
+}
